847a21cdb8af7a8cdcd14eed4a4ea08b58a9ff34
Kleinman <dylan.kleinman@linquest.com>
2019-05-21 15:14:13 -0700

    Created all burger components and css

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
-
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render (){
         return (
             <Auxiliary>
-                <div>Burger</div>
-                <div>Build Controls</div>
+                <Burger/>
             </Auxiliary>
         );
     }
